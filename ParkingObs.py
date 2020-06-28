7e42a8640d0134981adbbf84a12449da83b6136f
HarshSuvarna <48185882+HarshSuvarna@users.noreply.github.com>
2020-06-28 20:03:17 +0530

    Car Parking MPC program

---
+++ ParkingObs.py
@@ -0,0 +1,85 @@
+import numpy as np
+from sim.SimParkObs import CarSim
+
+
+options={}
+options['FIG_SIZE']=[8,8]
+options['OBSTACLES']=True
+
+class ModelPredictiveControl:
+    def __init__(self):
+        self.horizon = 10
+        self.dt = 0.2
+
+        self.reference1 = [10,0,0]
+        self.reference2 = None #[10,10,90]
+        
+
+
+        self.x_obs = 5.0
+        self.y_obs = 0.1
+
+
+
+    def plant_model(self,prev_state,dt,pedal,steering):
+
+        #these values can be used to get the next values of X,Y,V & psi at time t+1
+
+        x_t = prev_state[0] #pevious value of X
+        y_t = prev_state[1] #previous value of Y
+        psi_t = prev_state[2] #previous angle of the car
+        v_t = prev_state[3] #previous value of velocity
+        a_t = pedal        #previous value of acceleration
+        beta = steering     #previous value of steering
+        
+        #formula to calculate the state of the car at time t+1
+
+        x_t_1 = x_t + (v_t*np.cos(psi_t))*dt
+        y_t_1 = y_t + (v_t*np.sin(psi_t))*dt
+        v_t_1 = v_t + a_t*dt - v_t/25.0
+        psi_t_1 = psi_t + (v_t*(np.tan(beta)/2.5))*dt
+
+        return [x_t_1, y_t_1, psi_t_1,v_t_1]
+
+
+    def cost_function(self,u,*args):
+
+        state = args[0]
+        ref = args [1]
+        cost = 0.0
+
+        #The optimizer will put out multiple sets of input array with different
+        # combination of input values. We put all set of the input values one
+        #by one in plant model and check the cost of that
+        #set of inputs provide by the optimizer
+        
+        for k in range(0,self.horizon):
+            
+            #punish the system if the car is far way from the goal
+            
+            state = self.plant_model(state,self.dt,u[k*2],u[(k*2)+1])
+            cost += (state[0] - ref[0])**2  #for X
+            cost += (state[1]-ref[1])**2  #for Y
+
+            cost += (state[2]-ref[2])**2   #for angle
+
+            cost += self.obstacle_cost(state[0],state[1]) #for obstacle
+
+        return cost
+
+    def obstacle_cost(self,x,y):
+        x1=self.x_obs
+        y1=self.y_obs
+        distance=((x-x1)**2+(y-y1)**2)**0.5 #calculating the distance betweeen obstacle an
+                                            # and car
+
+        if distance>2:     #if distance is less than 2 cost will be 50
+            return 15
+        else:
+            return (1/distance)*30  #if car goes too close, add more cost
+
+CarSim(options,ModelPredictiveControl)
+
+
+
+
---
+++ SimParkObs.py
@@ -0,0 +1,140 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+from scipy.optimize import minimize
+import time
+
+
+
+def CarSim(options,ModelPredictiveControl):
+    ###settings##################################################
+
+    #start = time.clock()
+
+    FIG_SIZE = options['FIG_SIZE']
+    obstacle=options['OBSTACLES']
+ 
+    mpc=ModelPredictiveControl()
+
+    no_of_inputs=2   #two inputs for acceleration and steering wheel angle
+
+
+    u=np.zeros(mpc.horizon*no_of_inputs)
+
+    bounds =[]
+
+    for i in range(mpc.horizon):
+        bounds += [[-1,1]]
+        bounds += [[-0.8,0.8]]
+
+    
+
+    ref_1=mpc.reference1
+    ref_2=mpc.reference2
+
+    ref = ref_1
+
+    state_i=np.array([[0,0,0,0]]) #list of states the car will be in
+    u_i=np.array([[0,0]])         #list on inputs the car will actually execute
+
+
+    sim_total=250 #number of times the mpc will make preictions
+    predicted_info=[state_i]  #this 3D array will have the values of states in all the 250 iterations
+
+
+    #ACTUAL CODE##############################
+
+    for i in range(1,sim_total+1):
+        start_time = time.time()
+        #optimization
+        u_solution = minimize(mpc.cost_function, u,(state_i[-1],ref),method='SLSQP',bounds=bounds,tol= 1e-5)
+
+        u = u_solution.x
+
+        print('Step ' + str(i) + ' of ' + str(sim_total) + '   Time ' + str(round(time.time() - start_time,5)))
+    
+        y = mpc.plant_model(state_i[-1],mpc.dt,u[0],u[1])  #y= output of the system(x,y,velocity,angle) NOTE:Only one input value is executed out of the 20 predicted ones
+        predicted_states=np.array([y])
+        for k in range(1,mpc.horizon):
+            predictions=mpc.plant_model(predicted_states[-1],mpc.dt,u[2*k],u[2*k+1])          #predcitons will have row matrix of preiction of the very next state
+            predicted_states = np.append(predicted_states,np.array([predictions]),0)    #this row matrix will be added as a row in predicted_state matrix which will have 20 rows
+                                                                                        #when it gets out of the loop
+        predicted_info+=[predicted_states]                      #predicted_info will have the 250 matrices of predicted states when oit gets out of the loop
+        state_i = np.append(state_i,np.array([y]),0)            #the state when we getny executing the first predicted input is now stored as a row in matrix state_i
+        u_i = np.append(u_i, np.array([(u[0],u[1])]),0)         #the one exectuted input value is stored in u_i 
+
+
+
+#########Display#################
+
+    fig= plt.figure(figsize=(FIG_SIZE[0],FIG_SIZE[1]))  #defining the output window
+
+    gs=gridspec.GridSpec(8,8)       #
+
+    ax= fig.add_subplot(gs[:8,:8])  #creating a plot named 'ax' and of size (8,8) 
+
+
+    plt.xlim(-3,17)                #car  initial postition will be 0,0. SO taking
+    plt.ylim(-3,17)                               #0,0 away from the meeting point of two axes
+
+
+    plt.xticks(np.arange(0,11,step=2))   #range of number on X and Y axes
+    plt.yticks(np.arange(0,11,step=2))
+
+    plt.title('MPC 2D')
+
+
+
+    car_width= 1.0
+    patch_car = mpatches.Rectangle((0,0), car_width, 2.5, fc = 'k', fill=False)  #creating a rectangle to repesent a car
+    patch_goal = mpatches.Rectangle((0,0),car_width, 2.5, fc='b',ls='dashdot', fill=False) #reacting a rectangle to represemt the parking space
+
+
+    ax.add_patch(patch_car)   #add car to the plot
+    ax.add_patch(patch_goal)  #add parking space to the plot
+
+    predict, =ax.plot([],[],'r--',linewidth=1)  #visual representation of pedicted path
+
+
+    
+
+    if obstacle:
+        patch_obs=mpatches.Circle((mpc.x_obs, mpc.y_obs),0.5)  #add a circular obstacle in the pat
+        ax.add_patch(patch_obs)
+
+    #this function updates the positon of the car   
+    def car_patch_pos(x, y, psi):               #The origin of the car is on the top left hand
+        #return [x,y]                           #side.The origin is set to zero. SO we have to
+        x_new = x - np.sin(psi)*car_width/2     # shift the car a little upward to aline it 
+        y_new = y + np.cos(psi)*car_width/2     # with the prediction plot    
+        return [x_new, y_new]
+
+
+    def update_plot(num):
+        patch_car.set_xy(car_patch_pos(state_i[num,0],state_i[num,1],state_i[num,2]))
+        patch_car.angle = np.rad2deg(state_i[num,2])-90
+
+
+        if (num <=130 or ref_2 == None):
+            patch_goal.set_xy(car_patch_pos(ref_1[0],ref_1[1],ref_1[2]))
+            patch_goal.angle = np.rad2deg(ref_1[2])-90
+
+        else:
+            patch_goal.set_xy(car_patch_pos(ref_1[0],ref_1[1],ref_1[2]))
+            patch_goal.angle = np.rad2deg(ref_2[2])-90
+
+        predict.set_data(predicted_info[num][:,0],predicted_info[num][:,1])  #visual representation of predicted path
+        
+    
+        return patch_car
+
+
+
+    car_animation= animation.FuncAnimation(fig, update_plot,frames=range(1,len(state_i)),interval=100,repeat=True, blit=False)
+    
+
+
+
+    plt.show()
